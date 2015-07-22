415728b505a2a64df392cd497ace347caf475c7e
rizotas <aslheyramirez@gmail.com>
2015-07-22 18:18:47 -0500

    wiki create service

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,1 +1,8 @@
 from django.conf.urls import patterns, url
+from waliki.settings import WALIKI_SLUG_PATTERN
+
+from .views import PageCreateView
+
+urlpatterns = patterns('waliki.rest.views',
+	url(r'^new$', PageCreateView.as_view() , name='page_new'),
+)
--- wiki/views.py
+++ wiki/views.py
@@ -1,1 +1,6 @@
-from rest_framework import viewsets
+
+from waliki.rest.views import PageCreateView as CreateView
+from .serializers import PageCreateSerializer as CreateSer
+
+class PageCreateView(CreateView):
+	serializer_class = CreateSer
