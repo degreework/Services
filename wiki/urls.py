4277a702da48a288cc4bdc0401b79fe0880751b0
rizotas <aslheyramirez@gmail.com>
2015-07-30 15:26:42 -0500

    add service to approve Requests

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,17 +1,29 @@
 from django.conf.urls import patterns, url, include
 from waliki.settings import WALIKI_SLUG_PATTERN
 
-from .views import PageCreateView, RequestListView
+from .views import PageCreateView, RequestListView, PageListView, PageRetrieveView, RequestApproveView, VersionView as PageVersionView
 
 from .receivers import *
 
 urlpatterns = patterns('waliki.rest.views',
-	#urls for wiki
+	#urls for get published wiki
+	url(r'^published$', PageListView.as_view() , name='page_published'),
+
+	#urls for wiki create
 	url(r'^new$', PageCreateView.as_view() , name='page_new'),
-	
+
 	#urls for request
 	url(r'^request/all$', RequestListView.as_view() , name='request_list'),
 
+	#urls for approve request
+	url(r'^request/approve/(?P<slug>' + WALIKI_SLUG_PATTERN + ')\.\.(?P<version>[0-9a-f\^]{4,40})$', RequestApproveView.as_view() , name='request_approve'),
+
+	#get page version
+	url(r'^(?P<slug>' + WALIKI_SLUG_PATTERN + ')/version/(?P<version>[0-9a-f\^]{4,40})/$', PageVersionView.as_view(), name='page_version'),
+	
+	#NOTE: THIS MUST BE LAST URL
+	#urls for get a wiki page,
+	url(r'^(?P<slug>' + WALIKI_SLUG_PATTERN + ')$', PageRetrieveView.as_view() , name='page_retrieve'),
 	
 	#url(r'^', include('waliki.rest.urls')),
 
