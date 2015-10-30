ad481b00a6771b1f6a6e8a802c13ac9241b43e2b
rizotas <aslheyramirez@gmail.com>
2015-10-30 10:13:27 -0500

    https://github.com/degreeworkunivalle/Front/issues/102

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 from degree.urls import routerDegree
 from users.urls import routerUser
@@ -73,4 +75,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
