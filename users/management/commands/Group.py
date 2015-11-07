006aa46d33a5833639d83c939a0d162f536a3520
rizotas <aslheyramirez@gmail.com>
2015-11-07 16:39:45 -0500

    add feedback to Group command

--- users/management/commands/Group.py
+++ users/management/commands/Group.py
@@ -10,18 +10,21 @@ class Command(BaseCommand):
     help = 'Create default Groups with Permissions in db'
 
     def handle(self, *args, **options):
+        self.stdout.write("Creating Groups in settings.DEFAULT_GROUP_NAME")
 
-    	try:
-    		for group in DEFAULT_GROUP_NAME.keys():
-    			new_group, created = Group.objects.get_or_create(name=group)
+        try:
+            for group in DEFAULT_GROUP_NAME.keys():
+                print group
+                new_group, created = Group.objects.get_or_create(name=group)
 
-    			for permission in DEFAULT_GROUP_NAME[group]:
-    				splited_permission = permission.split(" | ")
-    				permission = Permission.objects.get(name=splited_permission[2])
-    				new_group.permissions.add(permission)
+                for permission in DEFAULT_GROUP_NAME[group]:
+                    print '\t', permission
+                    splited_permission = permission.split(" | ")
+                    permission = Permission.objects.get(name=splited_permission[2])
+                    new_group.permissions.add(permission)
 
 
-	        self.stdout.write("Default Groups created")
-    	except Exception, e:
-    		self.stdout.write("Default Groups can't be created")
-    		print e
+            self.stdout.write("Default Groups created")
+        except Exception, e:
+            self.stdout.write("Default Groups can't be created")
+            print e
